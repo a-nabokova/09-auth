b5b8877aaa82044d586fa759df8b9c19015f8062
a-nabokova <itcw2s@gmail.com>
2025-10-19 22:28:59 +0300

    notes page fix

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -11,7 +11,7 @@ interface Props {
  
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { id } = params;
-    const note = await getSingleNote(id);
+    const note =  await getSingleNote(id);
 
  
   return {
